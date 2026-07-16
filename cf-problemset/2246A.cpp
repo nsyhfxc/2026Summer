625fbb70ce89bca2fb043cc89fa9b5cc19080d01
念碎夜画繁星辰 <1691034079@qq.com>
2026-07-16 17:27:06 +0800

    update

--- cf-problemset/2246A.cpp
+++ cf-problemset/2246A.cpp
@@ -1,7 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define endl '\n'
 using namespace std;
+using i64 = long long;
+using u64 = unsigned long long;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i & 1) cout << i + 1 << ' ';
+        else cout << i - 1 << ' ';
+    }
+    cout << endl;
+}
 
 int main() {
-    cout << "1" << endl;
-    return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
 }
